7f9fb290aa5945aa13ddcaddf8e43f67cc01755c
valentinebudanov <budanov.vv@gmail.com>
2021-09-17 12:18:19 +0300

    2

--- conftest.py
+++ conftest.py
@@ -34,7 +34,6 @@ def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
-
     request.addfinalizer(fin)
     return fixture
 
@@ -42,22 +41,3 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--target", action="store", default="target.json")
-
-
-def pytest_generate_tests(metafunc):
-    for fixture in metafunc.fixturenames:
-        if fixture.startswith("data_"):
-            testdata = load_from_module(fixture[5:])
-            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-        elif fixture.startswith("json_"):
-            testdata = load_from_json(fixture[5:])
-            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-
-
-def load_from_module(module):
-    return importlib.import_module("data.%s" % module).testdata
-
-
-def load_from_json(file):
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
-        return jsonpickle.decode(f.read())
--- fixture/session.py
+++ fixture/session.py
@@ -8,10 +8,11 @@ class SessionHelper:
         self.app.main_url()
         wd.find_element_by_name("username").clear()
         wd.find_element_by_name("username").send_keys(user)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("LoginForm").submit()
+        wd.find_element_by_css_selector('input[type="submit"]').click()
+        wd.find_element_by_name("password").click()
+        wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").send_keys(password)
+        wd.find_element_by_css_selector('input[type="submit"]').click()
 
     def ensure_login(self, user, password):
         if self.is_logged_in():
@@ -23,7 +24,7 @@ class SessionHelper:
 
     def get_logged_user_name(self):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//div/div[1]/form/b").text[1:-1]
+        return wd.find_element_by_xpath('//*[@id="breadcrumbs"]/ul/li/a').text
 
     def is_logged_in_as(self, user):
         return self.get_logged_user_name() == user
@@ -34,8 +35,8 @@ class SessionHelper:
 
     def logout(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user")
+        wd.find_element_by_css_selector('i[class="fa fa-angle-down ace-icon"]').click()
+        wd.find_element_by_css_selector('a[href="/mantisbt-2.25.2/logout_page.php"]').click()
 
     def ensure_logout(self):
         if self.is_logged_in():
