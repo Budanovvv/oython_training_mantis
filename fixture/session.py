c8668feb1428350f764376bc8bb7aa88fc9fe799
valentinebudanov <budanov.vv@gmail.com>
2021-09-17 10:36:24 +0300

    start

---
+++ fixture/session.py
@@ -0,0 +1,42 @@
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, user, password):
+        wd = self.app.wd
+        self.app.main_url()
+        wd.find_element_by_name("username").clear()
+        wd.find_element_by_name("username").send_keys(user)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_id("LoginForm").submit()
+
+    def ensure_login(self, user, password):
+        if self.is_logged_in():
+            if self.is_logged_in_as(user):
+                return
+            else:
+                self.logout()
+        self.login(user, password)
+
+    def get_logged_user_name(self):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text[1:-1]
+
+    def is_logged_in_as(self, user):
+        return self.get_logged_user_name() == user
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("user")
+
+    def ensure_logout(self):
+        if self.is_logged_in():
+            self.logout()
